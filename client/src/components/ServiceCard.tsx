f558a1021d6e3ccac2bd67e7577a3b6156d9e19b
sylviabarraud2@gmail.com <sylviabarraud2@gmail.com>
2024-12-06 10:47:05 +0100

    add: servicecard component

--- client/src/components/ServiceCard.tsx
+++ client/src/components/ServiceCard.tsx
@@ -2,23 +2,21 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import "/src/styles/Card.css";
 
-interface CardProps {
+interface ServiceCardProps {
   img: string;
   title: string;
   date: string;
   description: string;
   user: string;
-  state: string;
 }
 
-export default function Card({
+export default function ServiceCard({
   img,
   title,
   date,
   description,
   user,
-  state,
-}: CardProps) {
+}: ServiceCardProps) {
   return (
     <div className="component-card">
       <Dialog.Root>
@@ -47,10 +45,6 @@ export default function Card({
             <div className="Fieldset">
               <span className="Label">Utilisateur :</span>
               <p className="UserInfo">{user}</p>
-            </div>
-            <div className="Fieldset">
-              <span className="Label">État :</span>
-              <p className="UserInfo">{state}</p>
             </div>
             <div className="Fieldset">
               <span className="Label">Date :</span>
